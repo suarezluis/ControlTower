ee371f259b324e03a07fef72ca2dcc5ff9d8c0cc
Luis Suarez <suarezluis@pop-os.localdomain>
2019-09-22 21:36:08 -0500

    Fixed redux devtools

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,10 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import {composeWithDevTools} from 'remote-redux-devtools';
+
 import ReduxThunk from 'redux-thunk';
 
-export default store = createStore(
+export default (store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(ReduxThunk)),
-);
+));
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button, View, Text} from 'react-native';
+import {Button, View, Text, StyleSheet} from 'react-native';
+import {Input} from 'react-native-elements';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 
@@ -7,8 +8,26 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Home Screen</Text>
+        <Text>Welcome To Control Tower</Text>
+        <Input
+          label="Your Name"
+          containerStyle={styles.input}
+          placeholder="John Doe"
+          leftIcon={{type: 'font-awesome', name: 'user'}}
+        />
+        <Input
+          label="Your AMA number"
+          containerStyle={styles.input}
+          placeholder="000000000"
+          leftIcon={{type: 'font-awesome', name: 'plane'}}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    paddingTop: 90,
+  },
+});
